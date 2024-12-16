6b22f1be9bf5bf471e46bea074a276effd04dbf0
jinya1013 <gab102manda100@gmail.com>
2024-12-16 23:12:29 +0900

    refactor: remove unecessary functions

--- frontend/app/api/route/payment/route.ts
+++ frontend/app/api/route/payment/route.ts
@@ -2,38 +2,6 @@ import { db } from "@vercel/postgres";
 
 const client = await db.connect();
 
-// Function to get station name suggestions
-async function getStationChoices(partialStationName: string, isHiragana: boolean) {
-  const data = await client.sql`
-    SELECT * FROM mobility.get_updated_station_choices(${partialStationName}, ${isHiragana})
-  `;
-  return data.rows;
-}
-
-// Function to get shortest route
-async function getShortestRoute(from: string, to: string, isHiragana: boolean) {
-  const data = await client.sql`
-    SELECT * FROM mobility.get_shortest_route(${from}, ${to}, ${isHiragana})
-  `;
-  return data.rows;
-}
-
-// Function to get train lines and distance
-async function getLinesAndDistance(from: string, to: string, isHiragana: boolean) {
-  const data = await client.sql`
-    SELECT * FROM mobility.get_lines_and_distance(${from}, ${to}, ${isHiragana})
-  `;
-  return data.rows;
-}
-
-// Function to get fare prices
-async function getFarePrices(from: string, to: string, isHiragana: boolean) {
-  const data = await client.sql`
-    SELECT * FROM mobility.get_fare_prices(${from}, ${to}, ${isHiragana})
-  `;
-  return data.rows[0];
-}
-
 // Function to calculate route payment
 async function getRoutePayment(from: string, to: string, adults: number, children: number, isHiragana: boolean) {
   const data = await client.sql`
